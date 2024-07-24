b8cc030ca31df717ca31ddbc86608f059734ef39
matheusosan <matheussantanaoli@gmail.com>
2024-07-24 09:27:37 -0300

    type: add service type to tthe service

--- src/services/specialities.service.ts
+++ src/services/specialities.service.ts
@@ -1,4 +1,10 @@
-export const getAllUsers = async () => {
+export interface Service {
+  id: number;
+  name: string;
+  price: number;
+}
+
+export const getAllServices = async (): Promise<Service[]> => {
   const res = await fetch("http://localhost:8080/service");
   const data = await res.json();
   return data;
