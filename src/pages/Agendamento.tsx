650ae2316ddd23d07de35a167cf8dfafcfbb8c35
matheusosan <matheussantanaoli@gmail.com>
2024-07-24 09:28:46 -0300

    refactor: split customer and appointment details into components, and wrap logic in a custom hook

--- src/pages/Agendamento.tsx
+++ src/pages/Agendamento.tsx
@@ -1,39 +1,9 @@
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
-import { ChangeEvent, useEffect, useState } from "react";
-import { getAllUsers } from "../services/specialities.service";
-
-interface Service {
-  id: number;
-  name: string;
-  price: number;
-}
+import Costumer from "../components/Costumer";
+import Appointment from "../components/Appointment";
 
 export default function Agendamento() {
-  const [data, setData] = useState<Service[]>([]);
-  const [selectedService, setSelectedService] = useState<Service | undefined>(
-    undefined
-  );
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const data = await getAllUsers();
-        setData(data);
-      } catch (error) {
-        throw new Error("Failed to fetch users");
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    const selectedId = event.target.value;
-    const service = data.find((service) => service.id === Number(selectedId));
-    setSelectedService(service);
-  };
-
   return (
     <>
       <Header />
@@ -43,58 +13,10 @@ export default function Agendamento() {
         </h2>
 
         <div className="flex justify-evenly mt-8 w-[1670px] h-[630px] border border-[#D9D9D9]">
-          <div className="flex flex-col flex-1 px-60 py-12 gap-10 border-r">
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">Informe seu CPF</h2>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="___.___.__-__"
-                className="w-auto"
-              />
-            </div>
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">Nome</h2>
-              <p>John Doe</p>
-            </div>
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">Data de Nascimento</h2>
-              <p>01/01/2001</p>
-            </div>
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">Telefone</h2>
-              <p>(99)999999999</p>
-            </div>
-          </div>
-
-          <div className="flex flex-col flex-1 px-60 py-12 gap-10">
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">
-                Selecione a especialidade desejada
-              </h2>
-              <select onChange={handleSelectChange} className="w-64">
-                {data.map((service) => (
-                  <option key={service.id} value={service.id}>
-                    {service.name}
-                  </option>
-                ))}
-              </select>
-            </div>
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">Valor da Consulta</h2>
-              <p>{selectedService ? selectedService.price : "R$00,00"}</p>
-            </div>
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">Selecione uma data</h2>
-              <input type="date" name="" id="" />
-            </div>
-            <div className="gap-5">
-              <h2 className="font-bold text-xl">Selecione um horário</h2>
-              <input type="date" name="" id="" />
-            </div>
-          </div>
+          <Costumer />
+          <Appointment />
         </div>
+
         <div className="flex w-[1670px] justify-between">
           <Link
             to={"/"}
@@ -105,7 +27,6 @@ export default function Agendamento() {
           <button className="px-12 py-4 font-bold bg-[#42DA5B] text-white">
             AGENDAR
           </button>
-          <button onClick={() => console.log(selectedService)}>LOG</button>
         </div>
       </main>
     </>
