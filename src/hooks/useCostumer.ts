10a4902ef751e0b0569d317405336722820a8377
Matheus Santana <matheussantanaoli@gmail.com>
2024-08-06 00:52:20 -0300

    feat: create usecostumer hook

---
+++ src/hooks/useCostumer.ts
@@ -0,0 +1,45 @@
+import { findCostumerByCpf } from "../services/costumer.service";
+import { useQuery } from "@tanstack/react-query";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+const schema = z.object({
+  cpf: z.string().min(11).max(11),
+});
+
+type Schema = z.infer<typeof schema>;
+
+export const useCostumer = () => {
+  const { register, handleSubmit, watch } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  });
+
+  const cpfInput = watch("cpf");
+
+  const onSubmit = () => {
+    refetch();
+  };
+
+  const {
+    data: client,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery({
+    queryKey: ["userData"],
+    queryFn: () => findCostumerByCpf(cpfInput),
+    enabled: false,
+    retry: false,
+  });
+
+  return {
+    client,
+    isLoading,
+    isError,
+    onSubmit,
+    refetch,
+    register,
+    handleSubmit,
+  };
+};
