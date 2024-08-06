927f467c175f1bc62ee5424943cb900699a27378
Matheus Santana <matheussantanaoli@gmail.com>
2024-08-06 00:48:07 -0300

    chore: add form errors constants

---
+++ src/constants/form-errors.ts
@@ -0,0 +1,7 @@
+export const FORM_ERRORS_MESSAGES = {
+  NAME: "Preencha seu nome para realizar o cadastro.",
+  BIRTHDATE: "Preencha sua data de nascimento",
+  CPF: "Digite um CPF válido",
+  PHONENUMBER: "Digite um número de telefone válido",
+  EMAIL: "Digite um email válido",
+};
