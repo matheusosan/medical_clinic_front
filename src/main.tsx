210381fc90792d865107f1aee7068d0a4d921bf3
Matheus Santana <matheussantanaoli@gmail.com>
2024-08-06 00:50:53 -0300

    chore: add toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} />;
+      <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>
 );
