dbc404e48ee7e39cd7fcd0b12c1d94cf3fdb3e65
Matheus Santana <matheussantanaoli@gmail.com>
2024-08-06 00:51:22 -0300

    add createClient service

--- src/services/costumer.service.ts
+++ src/services/costumer.service.ts
@@ -1,13 +1,27 @@
 export interface Costumer {
-  id: number;
+  id?: number;
   name: string;
   email: string;
   phoneNumber: string;
   cpf: string;
+  birthDate: string;
 }
 
 export const findCostumerByCpf = async (cpf: string): Promise<Costumer> => {
   const res = await fetch(`http://localhost:8080/client/cpf/${cpf}`);
   const data = await res.json();
+  return data;
+};
+
+export const createClient = async (clientData: Costumer) => {
+  const res = await fetch("http://localhost:8080/client", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(clientData),
+  });
+
+  const data = res.json();
   return data;
 };
