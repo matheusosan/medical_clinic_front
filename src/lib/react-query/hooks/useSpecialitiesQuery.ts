854d741ab6838e0dccd0a82fe73c501e7a1e81d1
Matheus Santana <matheussantanaoli@gmail.com>
2024-08-17 12:13:59 -0300

    refactor: split getallservices into a new custom hook

---
+++ src/lib/react-query/hooks/useSpecialitiesQuery.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllServices } from "../../../api/services/specialities.service";
+import { QUERY_KEYS } from "../query-keys";
+
+export const useSpecialitiesQuery = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SPECIALITIES],
+    queryFn: () => getAllServices(),
+  });
+};
