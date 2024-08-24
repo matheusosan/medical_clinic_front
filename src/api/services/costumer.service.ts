8d7596175bf0487ff9f9cbf079aa3b1bce0b3fac
Matheus Santana <matheussantanaoli@gmail.com>
2024-08-24 12:05:38 -0300

    feat: create getprofile request

--- src/api/services/costumer.service.ts
+++ src/api/services/costumer.service.ts
@@ -1,3 +1,4 @@
+import Cookies from "js-cookie";
 export interface Costumer {
   id?: number;
   name: string;
@@ -24,4 +25,36 @@ export const createClient = async (clientData: Costumer) => {
     },
     body: JSON.stringify(clientData),
   });
+};
+export interface ClientResponseDTO {
+  id: number;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  cpf: string;
+  birthDate: string;
+  role: string;
+}
+
+export const getProfile = async (): Promise<ClientResponseDTO> => {
+  const token = Cookies.get("access_token");
+
+  if (!token) {
+    throw new Error("Token não encontrado");
+  }
+
+  const res = await fetch("http://localhost:8080/client/profile", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Falha ao buscar perfil do usuário");
+  }
+
+  const data = await res.json();
+  return data;
 };
