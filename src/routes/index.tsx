0e1b9d0d4d2b2b643e098e781a2e74336db60065
Matheus Santana <matheussantanaoli@gmail.com>
2024-08-24 12:04:49 -0300

    add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Agendamento from "../pages/Agendamento";
-import Cadastrar from "../pages/Signup";
 import HomePage from "../pages/Home";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile";
+import Cadastrar from "../pages/Signup";
 
 export const router = createBrowserRouter([
   {
@@ -16,5 +18,13 @@ export const router = createBrowserRouter([
   {
     path: "/cadastrar",
     element: <Cadastrar />,
+  },
+  {
+    path: "/perfil",
+    element: <Profile />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
