870ead9749cfa54610e1a34e2dd900d96e2cb51d
Matheus Santana <matheussantanaoli@gmail.com>
2024-09-09 11:49:25 -0300

    chore: add speciality and appointment selected date error constants

--- src/constants/form-errors.ts
+++ src/constants/form-errors.ts
@@ -5,4 +5,6 @@ export const FORM_ERRORS_MESSAGES = {
   PHONENUMBER: "Digite um número de telefone válido",
   EMAIL: "Digite um email válido",
   PASSWORD: "A senha deve conter no mínimo 6 dígitos",
+  SPECIALITY: "Selecione uma opção válida",
+  APPOINTMENT_SELECTED_DATE: "Selecione uma data para consulta",
 };
