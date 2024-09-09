6e11f7325abf12bb0b0e55a4512bd3b6435e08ce
Matheus Santana <matheussantanaoli@gmail.com>
2024-09-09 11:51:45 -0300

    test: add password verify to the test

--- src/pages/Signup/signup.spec.tsx
+++ src/pages/Signup/signup.spec.tsx
@@ -1,14 +1,15 @@
-import { expect, it, describe } from "vitest";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
-import { fireEvent, render, screen } from "../../utils/test-utils";
-import { FORM_ERRORS_MESSAGES } from "../../constants/form-errors";
+import { describe, expect, it } from "vitest";
 import Cadastrar from ".";
+import { FORM_ERRORS_MESSAGES } from "../../constants/form-errors";
+import { fireEvent, render, screen } from "../../utils/test-utils";
 import HomePage from "../Home";
 
 const newUser = {
   name: "teste1",
   birthDate: "2000-01-01",
   cpf: "99999999999",
+  password: "123456",
   phoneNumber: "51999999999",
   email: "mail@mail.com",
 };
@@ -29,6 +30,8 @@ describe("<Agendamento>", () => {
       /digite sua data de nascimento/i
     );
     const cpfInput = screen.getByPlaceholderText(/digite seu cpf/i);
+    const passwordInput = screen.getByPlaceholderText(/crie uma senha/i);
+
     const phoneInput = screen.getByPlaceholderText(
       /digite seu número de telefone/i
     );
@@ -38,6 +41,7 @@ describe("<Agendamento>", () => {
     fireEvent.change(nameInput, { target: { value: newUser.name } });
     fireEvent.change(birthInput, { target: { value: newUser.birthDate } });
     fireEvent.change(cpfInput, { target: { value: newUser.cpf } });
+    fireEvent.change(passwordInput, { target: { value: newUser.password } });
     fireEvent.change(phoneInput, { target: { value: newUser.phoneNumber } });
     fireEvent.change(emailInput, { target: { value: newUser.email } });
 
@@ -48,6 +52,8 @@ describe("<Agendamento>", () => {
     expect(nameInput).toHaveValue(newUser.name);
     expect(birthInput).toHaveValue(newUser.birthDate);
     expect(cpfInput).toHaveValue(newUser.cpf);
+    expect(passwordInput).toHaveValue(newUser.password);
+
     expect(phoneInput).toHaveValue(newUser.phoneNumber);
     expect(emailInput).toHaveValue(newUser.email);
     expect(homeText).toBeInTheDocument();
@@ -89,6 +95,10 @@ describe("<Agendamento>", () => {
       FORM_ERRORS_MESSAGES.BIRTHDATE
     );
     const cpfErrorMessage = await screen.findByText(FORM_ERRORS_MESSAGES.CPF);
+    const passwordErrorMessage = await screen.findByText(
+      FORM_ERRORS_MESSAGES.PASSWORD
+    );
+
     const phoneErrorMessage = await screen.findByText(
       FORM_ERRORS_MESSAGES.PHONENUMBER
     );
@@ -99,6 +109,7 @@ describe("<Agendamento>", () => {
     expect(nameErrorMessage).toBeInTheDocument();
     expect(birthDateErrorMessage).toBeInTheDocument();
     expect(cpfErrorMessage).toBeInTheDocument();
+    expect(passwordErrorMessage).toBeInTheDocument();
     expect(phoneErrorMessage).toBeInTheDocument();
     expect(emailErrorMessage).toBeInTheDocument();
   });
