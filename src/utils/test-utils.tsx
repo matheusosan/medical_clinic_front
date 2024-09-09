6139a58f13d92eb53f15b6c93f0819d9ba7e23b4
Matheus Santana <matheussantanaoli@gmail.com>
2024-09-09 11:40:40 -0300

    remove: test wrapper

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,11 +1,7 @@
 // src/test-utils.tsx
-import React, { ReactElement } from "react";
-import { render, RenderOptions } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { MemoryRouter, Route, Routes } from "react-router-dom";
-import HomePage from "../pages/Home";
-import Cadastrar from "../pages/Signup";
-import Agendamento from "../pages/Agendamento";
+import { render, RenderOptions } from "@testing-library/react";
+import React, { ReactElement } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,23 +10,6 @@ const queryClient = new QueryClient({
     },
   },
 });
-
-const TestRouter = ({
-  children,
-  initialEntries = ["/"],
-}: {
-  children: React.ReactNode;
-  initialEntries?: string[];
-}) => (
-  <MemoryRouter initialEntries={initialEntries}>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/cadastrar" element={<Cadastrar />} />
-      <Route path="/agendamento" element={<Agendamento />} />
-    </Routes>
-    {children}
-  </MemoryRouter>
-);
 
 const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -47,4 +26,4 @@ const customRender = (
 export * from "@testing-library/react";
 
 // Override render method
-export { customRender as render, TestRouter };
+export { customRender as render };
