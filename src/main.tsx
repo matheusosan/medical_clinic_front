f588ffcb9705059b0fb5d86f2a74f7b3a98c7112
Matheus Santana <matheussantanaoli@gmail.com>
2024-09-09 11:51:55 -0300

    adjust imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ToastContainer } from "react-toastify";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./index.css";
 import { router } from "./routes";
 
 import "react-toastify/dist/ReactToastify.css";
@@ -13,7 +13,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
       <ToastContainer />
     </QueryClientProvider>
   </React.StrictMode>
