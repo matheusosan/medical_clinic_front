31bb194543f4557210ba44258cdcb67592a6a50c
Matheus Santana <matheussantanaoli@gmail.com>
2024-09-13 12:23:39 -0300

    test: create tests and mocked responses for profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -62,45 +62,47 @@ function Profile() {
             {!appointments?.length && (
               <h2>Você ainda não realizou agendamentos</h2>
             )}
-            {appointments!.map((appointment) => (
-              <div
-                className="flex items-center w-full justify-between md:px-12 border-b border-slate-300 py-4 shadow-slate-300/75 shadow-md cursor-pointer hover:scale-105 transition-transform duration-500 relative"
-                key={appointment.id}
-              >
-                <div className="flex flex-col justify-center md:justify-start w-full md:flex-row gap-2 md:gap-12">
-                  <div className="flex flex-col items-center">
-                    <label className="font-bold text-xl">Especialidade</label>
-                    <h2>{appointment.service.name}</h2>
+            <ul>
+              {appointments!.map((appointment) => (
+                <div
+                  className="flex items-center w-full justify-between md:px-12 border-b border-slate-300 py-4 shadow-slate-300/75 shadow-md cursor-pointer hover:scale-105 transition-transform duration-500 relative"
+                  key={appointment.id}
+                >
+                  <div className="flex flex-col justify-center md:justify-start w-full md:flex-row gap-2 md:gap-12">
+                    <div className="flex flex-col items-center">
+                      <label className="font-bold text-xl">Especialidade</label>
+                      <h2>{appointment.service.name}</h2>
+                    </div>
+                    <div className="flex flex-col items-center">
+                      <label className="font-bold text-xl">Valor</label>
+                      <h2>{toBRL(appointment.service.price)}</h2>
+                    </div>
+                    <div className="flex flex-col items-center">
+                      <label className="font-bold text-xl">Status</label>
+                      <h2 className={getStatusColor(appointment.status)}>
+                        {appointment.status}
+                      </h2>
+                    </div>
+                    <div className="flex flex-col items-center">
+                      <label className="font-bold text-xl">Data agendada</label>
+                      <h2>{formatDateToBRL(appointment.dataAgendada)}</h2>
+                    </div>
                   </div>
-                  <div className="flex flex-col items-center">
-                    <label className="font-bold text-xl">Valor</label>
-                    <h2>{toBRL(appointment.service.price)}</h2>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <label className="font-bold text-xl">Status</label>
-                    <h2 className={getStatusColor(appointment.status)}>
-                      {appointment.status}
-                    </h2>
-                  </div>
-                  <div className="flex flex-col items-center">
-                    <label className="font-bold text-xl">Data agendada</label>
-                    <h2>{formatDateToBRL(appointment.dataAgendada)}</h2>
-                  </div>
+                  {appointment.status !== "CANCELED" && (
+                    <button
+                      className="absolute top-4 right-4 md:static"
+                      onClick={() => mutate(appointment.id)}
+                    >
+                      {isPending ? (
+                        <LoaderCircle className="animate-spin" />
+                      ) : (
+                        <Trash2 color="red" />
+                      )}
+                    </button>
+                  )}
                 </div>
-                {appointment.status !== "CANCELED" && (
-                  <button
-                    className="absolute top-4 right-4 md:static"
-                    onClick={() => mutate(appointment.id)}
-                  >
-                    {isPending ? (
-                      <LoaderCircle className="animate-spin" />
-                    ) : (
-                      <Trash2 color="red" />
-                    )}
-                  </button>
-                )}
-              </div>
-            ))}
+              ))}
+            </ul>
           </div>
         </section>
       </div>
