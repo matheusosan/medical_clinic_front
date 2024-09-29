c13f7be1037e4e5b57426cf1cb66cedf7d6f950f
Matheus Santana <matheussantanaoli@gmail.com>
2024-09-29 19:17:47 -0300

    Merge last prod ready (#11)
    * test: create tests and mocked responses for profile page (#1)
    
    * Update workflow.yml
    
    * Refactor/token (#2)
    
    * test: create tests and mocked responses for profile page
    
    * refactor: split cookie functions into a util file
    
    * feat: create cpf input mask and cleancpf functions, also refactor services and components to use him (#3)
    
    * adjustments for login and profile page (#6)
    
    * Merge (#9)
    
    * Dev (#7)
    
    * test: create tests and mocked responses for profile page (#1)
    
    * Update workflow.yml
    
    * Refactor/token (#2)
    
    * test: create tests and mocked responses for profile page
    
    * refactor: split cookie functions into a util file
    
    * feat: create cpf input mask and cleancpf functions, also refactor services and components to use him (#3)
    
    * adjustments for login and profile page (#6)
    
    * Dev (#8)
    
    * test: create tests and mocked responses for profile page (#1)
    
    * Update workflow.yml
    
    * Refactor/token (#2)
    
    * test: create tests and mocked responses for profile page
    
    * refactor: split cookie functions into a util file
    
    * feat: create cpf input mask and cleancpf functions, also refactor services and components to use him (#3)
    
    * adjustments for login and profile page (#6)
    
    * add footer with credits and version (#10)

---
+++ src/pages/Home/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 flex items-center justify-end w-full pr-8">
+      <p className="font-bold">
+        Made by{" "}
+        <a
+          href="https://linkedin.com/in/matheusosan"
+          target="_blank"
+          className="text-[#0B4FFF]"
+        >
+          Matheus Oliveira
+        </a>{" "}
+        v-1.0
+      </p>
+    </footer>
+  );
+}
