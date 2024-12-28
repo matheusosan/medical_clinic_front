78c02dda8a0fdb083d4fcf05a2f54321b60ab069
matheusosan <matheussantanaoli@gmail.com>
2024-12-28 02:15:31 -0300

    adjust vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     include: ["src/**/*.{test,spec}.{js,ts,jsx,tsx}"],
     exclude: ["node_modules", "test/e2e/**/*", "dist"],
+    passWithNoTests: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
     globals: true,
